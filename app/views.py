684a1526041ab477bab79b1273848e03d15604bd
ytung <ytung@mit.edu>
2015-01-15 21:35:10 -0500

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.template.context import RequestContext
+from django.shortcuts import render, render_to_response
+
+
+def index(request):
+	social = None
+	if request.user and hasattr(request.user, "social_auth"):
+		social = request.user.social_auth.get(provider="facebook")
+	context = RequestContext(request, {"user": request.user, "request": request, "social": social})
+	return render_to_response("app/index.html", context_instance=context)
+
+def logout_view(request):
+	return render(request, "app/index.html")
