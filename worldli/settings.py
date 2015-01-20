ccd37fa8151d80cdcf1b6cb4b252aa8db629df4a
ytung <ytung@mit.edu>
2015-01-20 18:39:57 -0500

    settings database

--- worldli/settings.py
+++ worldli/settings.py
@@ -109,16 +109,6 @@ SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'user_friends']
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "db",
-        "USER": "yi-shiuantung",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
-
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
