18b17767c785224b4169ff753582126c8608342a
ytung <ytung@mit.edu>
2015-01-20 17:34:45 -0500

    database settings

--- worldli/settings.py
+++ worldli/settings.py
@@ -112,7 +112,8 @@ DATABASES['default'] =  dj_database_url.config()
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "yi-shiuantung",
+        "NAME": "app",
+        "USER": "ytung",
         "HOST": "",
     }
 }
